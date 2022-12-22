580527f0310529824974aa2db80117a1bc1edf80
huseynManaf <huseynim@code.edu.az>
2022-12-22 12:13:41 -0800

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
